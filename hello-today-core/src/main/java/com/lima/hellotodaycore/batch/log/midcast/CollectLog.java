b492961ae292b00008430090c37c8500701f95c5
LIMA\kimyo <kimyoulim1016@gmail.com>
2023-08-15 01:45:34 +0900

    DateTimeUtils 작성

--- hello-today-core/src/main/java/com/lima/hellotodaycore/batch/log/midcast/CollectLog.java
+++ hello-today-core/src/main/java/com/lima/hellotodaycore/batch/log/midcast/CollectLog.java
@@ -3,6 +3,7 @@ package com.lima.hellotodaycore.batch.log.midcast;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.lima.hellotodaycore.batch.log.ResponseType;
 import com.lima.hellotodaycore.common.connection.http.HttpConnection;
+import com.lima.hellotodaycore.common.utils.DateTimeUtils;
 import com.lima.hellotodaycore.common.utils.JsonUtils;
 import com.lima.hellotodaycore.kafka.producer.KafkaProducerConfig;
 import java.io.BufferedReader;
@@ -12,7 +13,6 @@ import java.net.HttpURLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 
@@ -30,7 +30,7 @@ public class CollectLog {
       urlBuilder.append("&").append(URLEncoder.encode("numOfRows", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("10", StandardCharsets.UTF_8)); /*한 페이지 결과 수*/
       urlBuilder.append("&").append(URLEncoder.encode("dataType", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("JSON", StandardCharsets.UTF_8)); /*요청자료형식(XML/JSON)Default: XML*/
       urlBuilder.append("&").append(URLEncoder.encode("stnId", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("108", StandardCharsets.UTF_8)); /*108 전국, 109 서울, 인천, 경기도 등 (활용가이드 하단 참고자료 참조)*/
-      urlBuilder.append("&").append(URLEncoder.encode("tmFc", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode("201310170600", StandardCharsets.UTF_8)); /*-일 2회(06:00,18:00)회 생성 되며 발표시각을 입력 YYYYMMDD0600 (1800)-최근 24시간 자료만 제공*/
+      urlBuilder.append("&").append(URLEncoder.encode("tmFc", StandardCharsets.UTF_8)).append("=").append(URLEncoder.encode(DateTimeUtils.convertLocalDateNow(DateTimeUtils.yyyyMMdd)+"0600", StandardCharsets.UTF_8)); /*-일 2회(06:00,18:00)회 생성 되며 발표시각을 입력 YYYYMMDD0600 (1800)-최근 24시간 자료만 제공*/
 
       HttpURLConnection conn = HttpConnection.getHttpURLConnection(urlBuilder);
 
---
+++ hello-today-core/src/main/java/com/lima/hellotodaycore/common/utils/DateTimeUtils.java
@@ -0,0 +1,15 @@
+package com.lima.hellotodaycore.common.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtils {
+
+  public static final String yyyyMMdd = "yyyyMMdd";
+
+  public static String convertLocalDateNow(String dateFormat) {
+    LocalDate currentDate = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+    return currentDate.format(formatter);
+  }
+}
