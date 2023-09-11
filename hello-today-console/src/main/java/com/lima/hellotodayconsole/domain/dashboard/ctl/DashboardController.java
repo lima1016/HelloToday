8df1067de6d2c3945e5066c8c7071a7c360086c0
LIMA\kimyo <kimyoulim1016@gmail.com>
2023-09-12 00:08:26 +0900

    front images and update crossOrigin

--- hello-today-console/src/main/java/com/lima/hellotodayconsole/domain/dashboard/ctl/DashboardController.java
+++ hello-today-console/src/main/java/com/lima/hellotodayconsole/domain/dashboard/ctl/DashboardController.java
@@ -1,17 +1,15 @@
 package com.lima.hellotodayconsole.domain.dashboard.ctl;
 
-import com.lima.hellotodayconsole.common.HelloJsonResponse;
+import com.lima.hellotodayconsole.common.response.HelloJsonResponse;
 import com.lima.hellotodayconsole.domain.dashboard.svc.DashboardService;
 import com.mongodb.client.FindIterable;
 import org.bson.Document;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dashboard")
-@CrossOrigin(origins = "http://localhost:3000")
 public class DashboardController {
 
   private DashboardService dashboardService;
