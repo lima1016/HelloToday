027cc59d91122bdfc5493c4b6c6fc3ea333edbad
YoulimKim <kimyoulim1016@gmail.com>
2025-02-16 13:33:22 +0900

    mongo 삭제

--- hello-today-core/src/main/java/com/lima/hellotodaycore/common/config/RegisterBeans.java
+++ hello-today-core/src/main/java/com/lima/hellotodaycore/common/config/RegisterBeans.java
@@ -1,18 +1,11 @@
 package com.lima.hellotodaycore.common.config;
 
-import com.lima.hellotodaycore.common.db.mongo.MongoConnection;
 import com.lima.hellotodaycore.kafka.producer.KafkaProducerConfig;
-import com.mongodb.client.MongoDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RegisterBeans {
-
-  @Bean
-  public static MongoDatabase mongoConnectionBean() {
-    return MongoConnection.getInstance();
-  }
 
   @Bean
   public static KafkaProducerConfig kafkaProducerBean() {
